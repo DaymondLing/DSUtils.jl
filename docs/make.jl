f2e6cfb170a815110152fc701ef30f37a92be0d6
Daymond Ling <daymond.ling@gmail.com>
2020-12-31 21:35:43 -0500

    Update documentation

--- docs/make.jl
+++ docs/make.jl
@@ -25,26 +25,5 @@ makedocs(;
 
 deploydocs(;
    repo="github.com/DaymondLing/DSUtils.jl",
+   devbranch = "main"
 )
-
-
-#=
-makedocs(;
-    modules=[DSUtils],
-    authors="Daymond Ling",
-    repo="https://github.com/DaymondLing/DSUtils.jl/blob/{commit}{path}#L{line}",
-    sitename="DSUtils.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://DaymondLing.github.io/DSUtils.jl",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-    ],
-)
-
-deploydocs(;
-    repo="github.com/DaymondLing/DSUtils.jl",
-)
-=#
